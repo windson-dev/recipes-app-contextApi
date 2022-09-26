f9a4ae9ef1d3f340c98c42f336228f8cdf4c5b41
João Renato <nateaorooj@gmail.com>
2022-09-26 16:34:32 -0300

    requisito 11

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import fetchApi from '../services/serviceAPI';
 
 function SearchBar() {
+  const [state, setState] = useState({ input: '', search: '' });
+
+  function handleChange({ target: { name, value } }) {
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  }
+
+  async function handleClick() {
+    if (state.input.length > 1 && state.search === 'f') {
+      global.alert('Your search must have only 1 (one) character');
+
+      return;
+    }
+
+    const data = await fetchApi(state.input, state.search);
+
+    console.log(data);
+  }
+
   return (
     <>
-      <input data-testid="search-input" type="text" />
+      <input
+        value={ state.input }
+        name="input"
+        onChange={ handleChange }
+        data-testid="search-input"
+        type="text"
+      />
 
       <label htmlFor="ingredient">
         ingredients:
         <input
+          name="search"
+          value="i"
+          onChange={ handleChange }
           type="radio"
           data-testid="ingredient-search-radio"
         />
@@ -15,6 +43,9 @@ function SearchBar() {
       <label htmlFor="nameSearch">
         nameSearch:
         <input
+          name="search"
+          value="s"
+          onChange={ handleChange }
           type="radio"
           data-testid="name-search-radio"
         />
@@ -22,6 +53,9 @@ function SearchBar() {
       <label htmlFor="firstLatter">
         firstLatter:
         <input
+          name="search"
+          value="f"
+          onChange={ handleChange }
           type="radio"
           data-testid="first-letter-search-radio"
         />
@@ -30,6 +64,7 @@ function SearchBar() {
       <button
         type="button"
         data-testid="exec-search-btn"
+        onClick={ handleClick }
       >
         Search
       </button>
--- src/services/serviceAPI.js
+++ src/services/serviceAPI.js
@@ -1,8 +1,12 @@
-// const api = async (endpointID) => {
-//   const URL = `https://themealdb.com/api/json/v1/1/lookup.php?i=${endpointID}`;
-//   const response = await fetch(URL);
-//   const data = await response.json();
-//   return data.meals[0].idMeal;
-// };
+async function fetchApi(input, search) {
+  const url = search === 'i'
+    ? `https://www.themealdb.com/api/json/v1/1/filter.php?i=${input}`
+    : `https://www.themealdb.com/api/json/v1/1/search.php?${search}=${input}`;
 
-// export default api;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+}
+
+export default fetchApi;
