e59e967101cac9914ba859be5d65547e480c84db
Windson Macedo <windsondev@hotmail.com>
2022-09-27 17:44:37 -0300

    requisito 24

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import Drinks from './components/Drinks';
 import Profile from './components/Profile';
 import DoneRecipes from './components/DoneRecipes';
 import FavoriteRecipes from './components/FavoriteRecipes';
-import Meal from './components/Meal';
-import Drink from './components/Drink';
+// import Meal from './components/Meal';
+// import Drink from './components/Drink';
+import RecipeDetails from './components/RecipeDetails';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -21,8 +22,8 @@ function App() {
             <Route exact path="/" component={ Login } />
             <Route exact path="/Meals" component={ Meals } />
             <Route exact path="/Drinks" component={ Drinks } />
-            <Route exact path="/Meals/:id" component={ Meal } />
-            <Route exact path="/Drinks/:id" component={ Drink } />
+            <Route exact path="/Meals/:id" component={ RecipeDetails } />
+            <Route exact path="/Drinks/:id" component={ RecipeDetails } />
             <Route exact path="/Profile" component={ Profile } />
             <Route exact path="/Done-Recipes" component={ DoneRecipes } />
             <Route exact path="/Favorite-Recipes" component={ FavoriteRecipes } />
