3d631b551232c3bc7f088b2bd7b359d88cba5171
João Renato <nateaorooj@gmail.com>
2022-10-03 19:34:24 -0300

    requisitos 58 e 59

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,10 +3,13 @@ import Footer from './Footer';
 import Header from './Header';
 
 function Profile() {
+  const email = localStorage
+    .getItem('user') ? JSON.parse(localStorage.getItem('user')).email : '';
+
   return (
     <div>
       <Header title="Profile" />
-      <p data-testid="profile-email" />
+      <p data-testid="profile-email">{email}</p>
       <button data-testid="profile-done-btn" type="button">Done Recipes</button>
       <button data-testid="profile-favorite-btn" type="button">Favorite Recipes</button>
       <button data-testid="profile-logout-btn" type="button">Logout</button>
