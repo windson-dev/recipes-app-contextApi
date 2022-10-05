aa35300aa4857671454f0e6c68a9bee9da44c134
João Renato <nateaorooj@gmail.com>
2022-10-05 00:28:31 -0300

    corrige erros de lint

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { mockedDoneRecipes } from './MOCK/mockedParse';
-import DoneRecipes from '../components/DoneRecipes';
+import renderWithRouter from './rwr/renderWithRouter';
+import App from '../App';
+
+const doneRecipesRoute = '/done-recipes';
+const drinkName = '155 Belmont';
+const mealName = 'Chakchouka';
 
 describe('testa o componente DoneRecipes possuíndo duas receitas finalizadas', () => {
   beforeEach(() => jest
@@ -10,29 +15,53 @@ describe('testa o componente DoneRecipes possuíndo duas receitas finalizadas', 
     .mockReturnValue(JSON.stringify(mockedDoneRecipes)));
 
   it('testa se renderiza as duas receitas', () => {
-    render(<DoneRecipes />);
+    renderWithRouter(<App />, [doneRecipesRoute]);
 
     userEvent.click(screen.getByRole('button', { name: 'All' }));
 
     expect(screen.getAllByRole('img')).toHaveLength(2);
   });
 
-  it('testa se renderiza apenas a bebida', () => {
-    render(<DoneRecipes />);
+  it('testa se renderiza apenas a bebida e o link da receita é copiado', () => {
+    Object.assign(navigator, {
+      clipboard: {
+        writeText: () => {},
+      },
+    });
+
+    jest.spyOn(navigator.clipboard, 'writeText');
+
+    renderWithRouter(<App />, [doneRecipesRoute]);
 
     userEvent.click(screen.getByRole('button', { name: 'Drinks' }));
 
-    expect(screen.getByAltText('155 Belmont')).toBeDefined();
-    expect(screen.queryByAltText('Chakchouka')).toBeNull();
+    expect(screen.getByAltText(drinkName)).toBeDefined();
+    expect(screen.queryByAltText(mealName)).toBeNull();
+
+    userEvent.click(screen.getByAltText('shareIcon'));
+
+    expect(navigator.clipboard.writeText).toHaveBeenCalledTimes(1);
   });
 
-  it('testa se renderiza apenas a comida', () => {
-    render(<DoneRecipes />);
+  it('testa se renderiza apenas a comida e o link da receita é copiado', () => {
+    Object.assign(navigator, {
+      clipboard: {
+        writeText: () => {},
+      },
+    });
+
+    jest.spyOn(navigator.clipboard, 'writeText');
+
+    renderWithRouter(<App />, [doneRecipesRoute]);
 
     userEvent.click(screen.getByRole('button', { name: 'Meals' }));
 
-    expect(screen.getByAltText('Chakchouka')).toBeDefined();
-    expect(screen.queryByAltText('155 Belmont')).toBeNull();
+    expect(screen.getByAltText(mealName)).toBeDefined();
+    expect(screen.queryByAltText(drinkName)).toBeNull();
+
+    userEvent.click(screen.getByAltText('shareIcon'));
+
+    expect(navigator.clipboard.writeText).toHaveBeenCalledTimes(1);
   });
 });
 
@@ -40,7 +69,7 @@ describe('testa o componente DoneRecipes não possuíndo nenhuma receita finaliz
   it('testa se não renderiza nenhuma receita', () => {
     jest.spyOn(Storage.prototype, 'getItem').mockReturnValue(null);
 
-    render(<DoneRecipes />);
+    renderWithRouter(<App />, [doneRecipesRoute]);
 
     expect(screen.queryAllByRole('img')).toHaveLength(0);
   });
